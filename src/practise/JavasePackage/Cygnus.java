ee5ce56fc11889b33bbfa4f5d6c12ed638c27038
jay958718389@qq.com <Tj199602>
2019-12-07 14:42:52 +0800

    init

---
+++ src/practise/JavasePackage/Cygnus.java
@@ -0,0 +1,14 @@
+package practise.JavasePackage;
+
+public   class  Cygnus{
+    static int value = 9; //注意value是静态的成员变量
+    public static void main(String[] args) throws Exception{
+        new Cygnus().printValue();
+    }
+
+    private   void printValue(){
+        int value = 69;
+        System.out.println(this.value);
+    }
+}
+//9   this为实例的引用    题中this.value相当于 Cygnus.value
