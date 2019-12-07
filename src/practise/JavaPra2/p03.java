43176ca0ec54cace2fb0b4d17f76d212a32a6bf3
jay958718389@qq.com <Tj199602>
2019-12-07 15:14:41 +0800

    init

--- src/practise/JavaPra2/p03.java
+++ src/practise/JavaPra2/p03.java
@@ -3,6 +3,6 @@ package practise.JavaPra2;
 public class p03 {
     public static void  main(String[] args){
         System.out.println("八进制："+Integer.toOctalString(10011101));
-        System.out.println("八进制："+Integer.toHexString(10011101));
+        System.out.println("十六进制："+Integer.toHexString(10011101));
     }
 }
