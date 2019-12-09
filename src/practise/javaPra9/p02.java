b8daefea9446dc91c902d87f87d257facf576a34
jay958718389@qq.com <Tj199602>
2019-12-09 20:44:14 +0800

    init

--- src/practise/javaPra9/p02.java
+++ src/practise/javaPra9/p02.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
  */
 public class p02 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入原始数据：");
         int num = sc.nextInt();
