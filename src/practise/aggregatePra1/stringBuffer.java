4c3ebfdd9c41a7331c0af2779995c08fcf7c4f8b
jay958718389@qq.com <Tj199602>
2019-12-12 09:28:12 +0800

    init

--- src/practise/aggregatePra1/stringBuffer.java
+++ src/practise/aggregatePra1/stringBuffer.java
@@ -2,9 +2,15 @@ package practise.aggregatePra1;
 
 public class stringBuffer {
     public static void main(String[] args) {
-        StringBuffer str = new StringBuffer("132");
-        str.append("abcdefghigklmnopqrstuvwxyz");
+        StringBuffer str = new StringBuffer();
+        char a = 'a';
+
+        for (int i = 0; i < 26; i++) {
+            str.append(a);
+            a = (char) (a + 1);
+
+        }
         System.out.println(str.reverse());
-        System.out.println(str.delete(0,5));
+        System.out.println(str.delete(0, 5));
     }
 }
