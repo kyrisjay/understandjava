12dfa4b99939a76275474b528368b7abd5a6be1c
jay958718389@qq.com <Tj199602>
2019-12-23 10:15:25 +0800

    init

--- src/practise/exampletwo/file/FileDemo.java
+++ src/practise/exampletwo/file/FileDemo.java
@@ -2,14 +2,12 @@ package practise.exampletwo.file;
 
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
+import javax.imageio.stream.ImageInputStream;
 import java.io.File;
 import java.io.FilenameFilter;
 
 public class FileDemo {
-
     public static void getFile(File file) {
-
-
         if (file.isDirectory()) {
             File[] file1 = file.listFiles();
             for (int i = 0; i < file1.length; i++) {
@@ -22,12 +20,10 @@ public class FileDemo {
         }
     }
 
-
     public static void main(String[] args) {
 
         File file = new File("/Users/edz/Downloads");
         getFile(file);
+
     }
-
-
 }
